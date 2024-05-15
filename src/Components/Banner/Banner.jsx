f1c7936231ef241b0e4964957c5d9ea0df21f8c7
Mohammad-imran75 <imranbd7531@gmail.com>
2024-05-15 21:59:12 +0600

    Swiper slider added in banner seciton

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,23 +1,49 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  EffectFade,
+  Autoplay
+} from "swiper/modules";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
+
 const Banner = () => {
   return (
-    <div
-      className="hero min-h-screen"
-      style={{
-        backgroundImage:
-          "url(https://i.ibb.co/rw5jqpg/side-view-traditional-turkish-iskender-doner-with-yogurt-clay-pot-wooden-board.jpg)",
-      }}
-    >
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello Client</h1>
-          <p className="mb-5">
-          Creating a digital platform connecting local farmers directly with consumers, promoting farm-to-table transparency and supporting sustainable food systems.
-          </p>
-          <button className="btn btn-primary bg-orange-700">Get Started</button>
-        </div>
-      </div>
-    </div>
+   <div>
+     <Swiper
+        modules={[Navigation, Pagination,Autoplay, Scrollbar, A11y, EffectFade]}
+        spaceBetween={50}
+        effect="flip"
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        autoplay={{ delay: 3000 }}
+
+      >
+    <SwiperSlide>
+          <img
+            style={{ width: "100%", height: "90vh" }}
+            src="https://i.ibb.co/Qfq1HCF/side-lamb-ragout-with-fried-onion-carrot-tomato-sauce-greens-vegetable-salad-table.jpg"
+            alt=""
+          />
+          <div className="lg:absolute top-[200px] left-[350px] ">
+            <h1 className="text-5xl text-rose-500 font-extrabold">
+            Komodo National Park <br /> <span>In Indonesia</span>
+            </h1>
+            <p className="text-xl lg:w-[500px] mx-auto font-semibold text-yellow-200 mt-3">
+            Komodo National Park in Indonesia is home to the iconic Komodo dragons and offers breathtaking landscapes of pristine beaches, crystal-clear waters, and rugged terrain.
+            </p>
+          </div>
+        </SwiperSlide>
+        </Swiper>
+   </div>
   );
 };
 
