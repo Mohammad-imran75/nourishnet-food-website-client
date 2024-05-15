8ba8a6fac6789ba732d11438a702ac13e5aaf6cb
Mohammad-imran75 <imranbd7531@gmail.com>
2024-05-15 13:20:01 +0600

    review section added

--- src/Components/Navbar/Home.jsx
+++ src/Components/Navbar/Home.jsx
@@ -1,20 +1,21 @@
 import { useLoaderData } from "react-router-dom";
 import FoodCard from "../Available/FoodCard";
 import Banner from "../Banner/Banner";
+import Review from "../review/Review";
 
 const Home = () => {
   const allFoods = useLoaderData().slice(0, 6);
   console.log(allFoods);
   return (
     <div>
-        <Banner></Banner>
-      {
-        <div className="grid md:grid-cols-2  gap-5 p-5">
-          {allFoods?.map((food) => (
-            <FoodCard key={food._id} food={food}></FoodCard>
-          ))}
-        </div>
-      }
+      <Banner></Banner>
+
+      <div className="grid md:grid-cols-2  gap-5 p-5">
+        {allFoods?.map((food) => (
+          <FoodCard key={food._id} food={food}></FoodCard>
+        ))}
+      </div>
+      <Review/>
     </div>
   );
 };
